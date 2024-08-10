b1f14b064171f55c6c27189ac203ad24da8882b6
Ed0z21 <79357635+Ed0z21@users.noreply.github.com>
2024-08-10 19:34:46 +0300

    Add files via upload

---
+++ Lesson1.py
@@ -0,0 +1,30 @@
+# 1st program
+
+i = (9**0.5) * 5
+print(i)
+
+# 2nd program
+
+a = 9.99
+b = 9.98
+c = 1000
+d = 1000.1
+
+print(a > b and c != d)
+
+# 3rd program
+
+q = 2 * 2 + 2
+p = 2 * (2 + 2)
+
+print(q, p, q == p)
+
+# 4th program
+
+k = '123.456'
+k1 = float(k)
+l = int(k1 * 10)
+l1 = l % 10
+
+print(l1)
+
